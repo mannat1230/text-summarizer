88325423abd4bae6d959306a8a0ef555f8a8b656
mannat1230 <80698527+mannat1230@users.noreply.github.com>
2025-04-15 17:20:12 +0000

    Fixing using both gradio Blocks and Interface in app

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 from transformers import pipeline
 import gradio as gr
 
-# define the model
+# Define the model
 model = pipeline("summarization")
 
-# define the predict function
+# Define the predict function
 def predict(prompt):
     summary = model(prompt)[0]["summary_text"]
     return summary
 
-
+# Using gr.Blocks() to create the interface
 with gr.Blocks() as demo:
     textbox = gr.Textbox(placeholder="Enter text block to summarize", lines=4)
-    gr.Interface(fn=predict, inputs=textbox, outputs='text')
+    output = gr.Textbox()
+    submit_btn = gr.Button("Summarize")
+    submit_btn.click(fn=predict, inputs=textbox, outputs=output)
 
 demo.launch()
-
