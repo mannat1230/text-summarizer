319da9f749f42c4dd0761747bf95c5dd3bd7cd4a
mannat1230 <80698527+mannat1230@users.noreply.github.com>
2025-04-15 16:23:16 +0000

    Initial working text summarizer app with CI/CD

---
+++ app.py
@@ -0,0 +1,18 @@
+from transformers import pipeline
+import gradio as gr
+
+# define the model
+model = pipeline("summarization")
+
+# define the predict function
+def predict(prompt):
+    summary = model(prompt)[0]["summary_text"]
+    return summary
+
+
+with gr.Blocks() as demo:
+    textbox = gr.Textbox(placeholder="Enter text block to summarize", lines=4)
+    gr.Interface(fn=predict, inputs=textbox, outputs='text')
+
+demo.launch()
+
